df31957651ac1ef0dfb40db61321efde8861c83e
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-28 11:30:17 +0000

    Update at10ex3.php

---
+++ at10/at10ex3.php
@@ -0,0 +1,27 @@
+<?php
+    $Dados = array(2, 5, 10, 'Maria', 2.5, 'Rui', 8, 'A',3,'B');
+echo "Valores do array:";
+for($i=0; $i<10; $i++)
+{
+    echo $Dados[$i]. " ";
+}
+echo "<br>------------------------------------------------------";
+echo '<br> Valores do array: <br>';
+for($i=0; $i<10; $i++)
+{
+    echo $Dados[$i]. "<br>";
+}
+echo "------------------------------------------------------<br>";
+$a[]=7;
+$a[]="Carlos";
+$a[1]="Pedro";
+$a[4]=50;
+$a[5]=Ruizino;
+$a[7]=" ";
+echo '<br>';
+echo "Valores do array:";
+for($i=0; $i<12; $i++)
+{
+    echo $Dados[$i]. " ";
+}
+?>
