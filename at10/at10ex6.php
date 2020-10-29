ff2901c85105c78d9f216573ac8c8e019b568733
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-29 11:32:07 +0000

    Update at10ex3.php, at10ex4.php, and 4 more files...

---
+++ at10/at10ex6.php
@@ -0,0 +1,19 @@
+<?php
+    $pacientes = array (
+        'Joana'=>20,
+        'Rui'=>25,
+        'Ana'=>30,
+        'Pedro'=>24,
+        'Carla'=>23
+    );
+        echo 'Idade do paciente Joana ' .$pacientes['Joana']. 'anos.';
+    
+
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+
+    foreach ($pacientes as $nome=>$idade) {
+        echo 'O paciente ' .$nome. ' tem ' .$idade. ' anos.<br>';
+    }
+?>
